52489af32071da2bc67f9d9ea7e193cfc547fdc1
andrew_jxhn <1520andrewaj@gmail.com>
2021-07-28 14:11:54 +0530

    Project Commit

---
+++ Project Files for Smart Parking/Extras-Backup/Parkingslots.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 24 10:54:39 2021
+
+@author: vighn
+"""
+import random
+import wiotp.sdk.device
+import time
+myConfig = { 
+    "identity": {
+        "orgId": "qqjb5y",
+        "typeId": "Device_1",
+        "deviceId":"1223"
+    },
+    "auth": {
+        "token": "12090009"
+    }
+}
+
+def myCommandCallback(cmd):
+    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+
+
+try:
+    client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+    client.connect()
+    
+    while True:
+        n = int(input())
+        myData = dict()
+        for i in range(1,n+1):
+            myData['parking'+str(i)] = None
+        for i in myData.keys():
+            myData[i] = random.randint(0,1)
+        myData['totalslots'] = n;
+        client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+        print("Published data Successfully: %s", myData)
+        client.commandCallback = myCommandCallback
+        time.sleep(2)
+except KeyboardInterrupt:
+    client.disconnect()
